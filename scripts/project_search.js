cd565626957186b5ce90437bb54225c547ece6cf
yukimaru32 <cyuuki15@gmail.com>
2024-12-07 17:18:08 -0700

    made some commitment

---
+++ scripts/project_search.js
@@ -0,0 +1,417 @@
+const mangas = [
+    {
+        mangaTitle: "Attack On Titan",
+        author: "Hajime,Isayama",
+        published: "2010,March,17",
+        sales: 140,
+        genre: "battle",
+        imageUrl:"images_project/attack_on_titan.jpg",
+        cite:"Attack On Titan Vol.1"
+      },
+    {
+        mangaTitle: "Assasination Class",
+        author: "Yusei,Matsui",
+        published: "2012,November,2",
+        sales: 25,
+        genre: "battle,romance",
+        imageUrl:"images_project/assasination_class.jpeg",
+        cite:"Assasination Class Vol.1"
+      },
+    {
+        mangaTitle: "Bleach",
+        author: "Taito,Kubo",
+        published: "2001,August,7",
+        sales: 130,
+        genre: "battle,horror",
+        imageUrl:"images_project/bleach.jpg",
+        cite:"Bleach Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+    {
+        mangaTitle: "",
+        author: "",
+        published: "",
+        sales: 100,
+        genre: "",
+        imageUrl:"images_project/",
+        cite:"Vol.1"
+      },
+
+    
+
+]
+
+
+function createMangaCard(mangas) {
+    const list = document.querySelector(".mangalist");
+    list.innerHTML = ""; // Clear existing content
+  
+      mangas.forEach(manga =>{
+        let card =document.createElement("section");
+        let sub1 =document.createElement("div");
+        let sub2 =document.createElement("div");
+        let name =document.createElement("h3");
+        let author=document.createElement("p");
+  
+        let publication=document.createElement("p");
+        let sale=document.createElement("p");
+        let genre=document.createElement("p");
+        let img=document.createElement("img");
+        let cite=document.createElement("cite");
+  
+        name.textContent =`Title: ${manga.mangaTitle}`;
+        author.innerHTML= `<span class="label">Author:</span> ${manga.author}`;
+        publication.innerHTML= `<span class="label">Published:</span> ${manga.published}`;
+        sale.innerHTML= `<span class="label">Total published:</span> ${manga.sales}M books`;
+        genre.innerHTML= `<span class="label">Genre:</span> ${manga.genre}`;
+        img.setAttribute("src",manga.imageUrl);
+        img.setAttribute("alt",`${manga.mangaTitle} image`);
+        img.setAttribute("loading","lazy");
+        cite.innerHTML= `<span class="label">Citation: ${manga.cite}`;
+  
+        sub1.appendChild(name);
+        sub1.appendChild(author);
+        sub1.appendChild(publication);
+        sub1.appendChild(sale);
+        sub1.appendChild(genre);
+        sub2.appendChild(img);
+        sub2.appendChild(cite);
+        card.appendChild(sub1);
+        card.appendChild(sub2);
+  
+        document.querySelector(".mangalist").appendChild(card);
+  
+      })
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    createMangaCard(mangas); // 初期データを表示
+    document.getElementById("home").addEventListener("click",()=>{
+      createMangaCard(mangas);
+    });
+
+  });
+  
