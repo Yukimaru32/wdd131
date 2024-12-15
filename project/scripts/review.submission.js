9f4c8c31e112e88ceb435ee1c4cb46bc02a5fed9
yukimaru32 <cyuuki15@gmail.com>
2024-12-14 23:40:04 -0700

    fixed some errors

---
+++ project/scripts/review.submission.js
@@ -0,0 +1,21 @@
+if (submit && reviewForm) {
+    submit.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        let review = parseInt(localStorage.getItem('reviewCount')) || 0;
+
+        review++;
+
+        localStorage.setItem('reviewCount', review);
+
+        reviewForm.reset();
+
+        window.location.href = 'confirmation.html';
+    });
+}
+
+
+if (reviewCount) {
+    const reviews = parseInt(localStorage.getItem('reviewCount')) || 0;
+    reviewCount.textContent = reviews;
+}
